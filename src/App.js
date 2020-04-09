48e7af894383082d6d001b6036f4224b6bcded53
beaucouplus <maxime@beaucouplus.com>
2020-04-09 17:24:17 +0200

    paper year calendar > first draft at cell creation algo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import React, { useEffect } from "react";
-import moment from "moment";
+import dayjs from "dayjs";
+import isLeapYear from "dayjs/plugin/isLeapYear";
+dayjs.extend(isLeapYear);
 
 function range(start, end) {
   if (start === end) return [start];
   return [start, ...range(start + 1, end)];
+}
+
+function createCells(year) {
+  const months = range(0, 11); // months are 0 based in calculations
+  const monthDays = range(1, 31);
+  const cells = [];
+
+  const daysPerMonth = {
+    1: 31,
+    2: dayjs(`${year}-01-01`).isLeapYear() ? 29 : 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+  };
+
+  months.forEach(function (month) {
+    monthDays.forEach(function (day) {
+      if (day > daysPerMonth[month + 1]) {
+        cells.push({
+          column: month,
+          row: day,
+          hasDate: false,
+          date: undefined,
+        });
+      } else {
+        cells.push({
+          column: month,
+          row: day,
+          hasDate: true,
+          date: new Date(year, month, day),
+        });
+      }
+    });
+  });
+  return cells;
 }
 
 function App() {
@@ -15,6 +59,10 @@ function App() {
     cellBorders: "border border-gray-500",
   };
 
+  const cells = createCells(year);
+
+  useEffect(() => console.log(cells));
+
   return (
     <div className="px-2 w-screen h-screen">
       <h1 className="font-semibold text-gray-700">Year plan {year}</h1>
@@ -22,14 +70,18 @@ function App() {
         <thead className={`${css.cellBorders}`}>
           <MonthsRow css={css} />
         </thead>
-        <tr>
-          {columns.map((x) => (
-            <td className={`${css.cellBorders} py-6`}>&nbsp;</td>
+        <tbody>
+          <tr>
+            {columns.map((x, id) => (
+              <td className={`${css.cellBorders} py-6`} key={id}>
+                &nbsp;
+              </td>
+            ))}
+          </tr>
+          {monthDays.map((monthDay) => (
+            <DaysRow monthDay={monthDay} year={year} key={monthDay} css={css} />
           ))}
-        </tr>
-        {monthDays.map((monthDay) => (
-          <DaysRow monthDay={monthDay} year={year} key={monthDay} css={css} />
-        ))}
+        </tbody>
       </table>
     </div>
   );
@@ -74,7 +126,7 @@ function DaysRow({ monthDay, year, css }) {
   const baseDate = `${year}-01-${twoDigitsMonthDay}`;
 
   function addMonths(months) {
-    const nextDate = moment(baseDate).clone().add(months, "months");
+    const nextDate = dayjs(baseDate).clone().add(months, "months");
     const formattedDate = {
       monthDay: nextDate.date(),
       weekDayName: nextDate.format("dddd"),
