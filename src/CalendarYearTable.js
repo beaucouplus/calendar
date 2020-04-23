2726112bbf355572860424eef490255f56a13738
beaucouplus <maxime@beaucouplus.com>
2020-04-23 17:31:41 +0200

    add event > first steps

--- src/CalendarYearTable.js
+++ src/CalendarYearTable.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import dayjs from "dayjs";
 import LocalizedFormat from "dayjs/plugin/localizedFormat";
-import { Modal, ModalStore, ModalContext } from "./Modal";
+import Modal from "./Modal";
 import { range, createCells } from "./utils";
+import { OutlineButton } from "./Button";
 
 dayjs.extend(LocalizedFormat);
 
@@ -11,13 +12,18 @@ function CalendarYearTable({ year }) {
   const monthDays = range(1, 31);
   const columns = range(1, 14);
   const css = { cellBorders: "border border-gray-500" };
-  const events = {
+  const dummyEvents = {
     "2020-04-13": [{ title: "go to the beach" }, { title: "play FF7" }],
     "2020-04-19": [{ title: "confinement" }],
     "2020-05-20": [{ title: "poule au pot" }],
   };
 
+  const [events, setEvents] = useState(dummyEvents);
   const cells = createCells(year, events);
+
+  function addEvent(event) {
+    setEvents({ ...events, ...event });
+  }
 
   return (
     <table className="table-fixed w-full border text-xs bg-white">
@@ -33,7 +39,14 @@ function CalendarYearTable({ year }) {
           ))}
         </tr>
         {monthDays.map((monthDay) => {
-          return <DaysRow days={cells[monthDay]} key={monthDay} css={css} />;
+          return (
+            <DaysRow
+              days={cells[monthDay]}
+              key={monthDay}
+              css={css}
+              onAddEvent={addEvent}
+            />
+          );
         })}
       </tbody>
     </table>
@@ -70,25 +83,28 @@ function MonthsRow({ css }) {
   );
 }
 
-function DaysRow({ days, css }) {
+function DaysRow({ days, css, onAddEvent }) {
   const tdClass = `${css.cellBorders} font-semibold text-center text-gray-700`;
   const monthDay = dayjs(days[0].date).format("D");
+
   return (
-    <ModalStore>
-      <tr>
-        <td className={tdClass}>{monthDay}</td>
-        {days.map((day, id) =>
-          day.hasDate ? (
-            <Modal key={id}>
-              <DayCell date={day.date} events={day.events} css={css} key={id} />
-            </Modal>
-          ) : (
-            <EmptyCell css={css} key={id} />
-          )
-        )}
-        <td className={tdClass}>{monthDay}</td>
-      </tr>
-    </ModalStore>
+    <tr>
+      <td className={tdClass}>{monthDay}</td>
+      {days.map((day, id) =>
+        day.hasDate ? (
+          <DayCell
+            date={day.date}
+            events={day.events}
+            css={css}
+            key={id}
+            onAddEvent={onAddEvent}
+          />
+        ) : (
+          <EmptyCell css={css} key={id} />
+        )
+      )}
+      <td className={tdClass}>{monthDay}</td>
+    </tr>
   );
 }
 
@@ -96,8 +112,9 @@ function EmptyCell({ css }) {
   return <td className={`${css.cellBorders}`}></td>;
 }
 
-function DayCell({ date, events, css }) {
-  const { onShowModal } = useContext(ModalContext);
+function DayCell({ date, events, css, onAddEvent }) {
+  const [showModal, setShowModal] = useState(false);
+  const closeModal = () => setShowModal(false);
 
   const weekday = dayjs(date).format("dddd");
 
@@ -123,12 +140,17 @@ function DayCell({ date, events, css }) {
   const tdStyle = getTDStyle();
 
   return (
-    <td
-      className={`px-2 ${css.cellBorders} ${tdStyle} font-semibold cursor-pointer`}
-      onClick={() => onShowModal(<EventList date={date} events={events} />)}
-    >
-      {weekday[0]}
-    </td>
+    <>
+      <td
+        className={`px-2 ${css.cellBorders} ${tdStyle} font-semibold cursor-pointer`}
+        onClick={() => setShowModal(true)}
+      >
+        {weekday[0]}
+      </td>
+      <Modal showModal={showModal} onCloseModal={closeModal}>
+        <EventList date={date} events={events} onAddEvent={onAddEvent} />
+      </Modal>
+    </>
   );
 }
 
@@ -138,7 +160,13 @@ DayCell.propTypes = {
   css: PropTypes.object,
 };
 
-function EventList({ date, events }) {
+function EventList({ date, events, onAddEvent }) {
+  const addEvent = () => {
+    onAddEvent({
+      [dayjs(date).format("YYYY-MM-DD")]: [{ title: "added event" }],
+    });
+  };
+
   return (
     <div className="mx-2">
       <h2 className="text-xl font-medium text-gray-800 leading-loose border-b-2 border-yellow-600">
@@ -147,8 +175,15 @@ function EventList({ date, events }) {
       <ul className="list-inside list-disc mt-2 text-gray-800">
         {events && events.map((event, idx) => <li key={idx}>{event.title}</li>)}
       </ul>
+      <OutlineButton callBack={addEvent}>Add Event</OutlineButton>
     </div>
   );
 }
+
+EventList.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  events: PropTypes.array,
+  onAddEvent: PropTypes.func.isRequired,
+};
 
 export default CalendarYearTable;
