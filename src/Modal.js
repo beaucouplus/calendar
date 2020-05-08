db2a192fe00842d992e88716547a26dfc9811882
Maxime Souillat <maxime@beaucouplus.com>
2020-05-08 11:39:57 +0200

    Make modal accessible (#24)
    * make modal accessible > aria labels + autoFocus on close modal button
    * make modal accessible > added closing by escape key
    * make modal accessible > added closing by clicking outside
    * make modal accessible > added focus react trap to trap focus inside modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,12 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
-import { OutlineButton } from "./Button";
+import FocusTrap from "focus-trap-react";
+import useOnclickOutside from "react-cool-onclickoutside";
+import { ModalButton } from "./Button";
 
 function Modal({ children, showModal, onCloseModal }) {
+  const modalContent = useRef();
+
+  useOnclickOutside(modalContent, () => {
+    onCloseModal();
+  });
+
   return (
     <>
       {showModal ? (
-        <ModalContent onCloseModal={onCloseModal}>{children}</ModalContent>
+        <ModalContent
+          onCloseModal={onCloseModal}
+          modalContentRef={modalContent}
+        >
+          {children}
+        </ModalContent>
       ) : (
         <React.Fragment />
       )}
@@ -14,23 +27,46 @@ function Modal({ children, showModal, onCloseModal }) {
   );
 }
 
-function ModalContent({ children, onCloseModal }) {
-  return ReactDOM.createPortal(
-    <aside
-      className="fixed top-0 left-0 w-screen h-screen flex items-center"
-      style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-    >
-      <div className="static box-border top-0 right-0 h-screen w-3/4 max-w-3/4 p-2 bg-white border shadow-md">
-        <div className="w-full flex items-center justify-end flex-wrap p-1">
-          <OutlineButton callBack={() => onCloseModal()}>✕</OutlineButton>
-        </div>
+function ModalContent({ children, onCloseModal, modalContentRef }) {
+  const onKeyDown = (event) => {
+    if (event.keyCode === 27) {
+      onCloseModal();
+    }
+  };
 
-        <div className="h-full box-border overflow-auto flex flex-col justify-start mx-2 px-1 pt-1 pb-5">
-          {children}
+  return ReactDOM.createPortal(
+    <FocusTrap>
+      <aside
+        className="fixed top-0 left-0 w-screen h-screen flex items-center"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+        role="dialog"
+        aria-modal="true"
+        tabIndex="-1"
+        onKeyDown={onKeyDown}
+      >
+        <div
+          ref={modalContentRef}
+          className="static box-border top-0 right-0 h-screen w-3/4 max-w-3/4 p-2 bg-white border shadow-md"
+        >
+          <div className="w-full flex items-center justify-end flex-wrap p-1">
+            <ModalButton
+              callBack={() => onCloseModal()}
+              ariaLabel="Close modal"
+              ariaLabelledby="close-modal"
+              autoFocus={true}
+            >
+              ✕
+            </ModalButton>
+          </div>
+
+          <div className="h-full box-border overflow-auto flex flex-col justify-start mx-2 px-1 pt-1 pb-5">
+            {children}
+          </div>
         </div>
-      </div>
-    </aside>,
+      </aside>
+    </FocusTrap>,
     document.body
   );
 }
+
 export default Modal;
