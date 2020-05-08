2ca01d2e7c48686b8f93b454fed6ce75c62af051
beaucouplus <maxime@beaucouplus.com>
2020-05-08 11:22:31 +0200

    make modal accessible > added focus react trap to trap focus inside  modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import FocusTrap from "focus-trap-react";
 import useOnclickOutside from "react-cool-onclickoutside";
 import { ModalButton } from "./Button";
+
 function Modal({ children, showModal, onCloseModal }) {
   const modalContent = useRef();
 
@@ -33,34 +35,36 @@ function ModalContent({ children, onCloseModal, modalContentRef }) {
   };
 
   return ReactDOM.createPortal(
-    <aside
-      className="fixed top-0 left-0 w-screen h-screen flex items-center"
-      style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-      role="dialog"
-      aria-modal="true"
-      tabIndex="-1"
-      onKeyDown={onKeyDown}
-    >
-      <div
-        ref={modalContentRef}
-        className="static box-border top-0 right-0 h-screen w-3/4 max-w-3/4 p-2 bg-white border shadow-md"
+    <FocusTrap>
+      <aside
+        className="fixed top-0 left-0 w-screen h-screen flex items-center"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+        role="dialog"
+        aria-modal="true"
+        tabIndex="-1"
+        onKeyDown={onKeyDown}
       >
-        <div className="w-full flex items-center justify-end flex-wrap p-1">
-          <ModalButton
-            callBack={() => onCloseModal()}
-            ariaLabel="Close modal"
-            ariaLabelledby="close-modal"
-            autoFocus={true}
-          >
-            ✕
-          </ModalButton>
-        </div>
+        <div
+          ref={modalContentRef}
+          className="static box-border top-0 right-0 h-screen w-3/4 max-w-3/4 p-2 bg-white border shadow-md"
+        >
+          <div className="w-full flex items-center justify-end flex-wrap p-1">
+            <ModalButton
+              callBack={() => onCloseModal()}
+              ariaLabel="Close modal"
+              ariaLabelledby="close-modal"
+              autoFocus={true}
+            >
+              ✕
+            </ModalButton>
+          </div>
 
-        <div className="h-full box-border overflow-auto flex flex-col justify-start mx-2 px-1 pt-1 pb-5">
-          {children}
+          <div className="h-full box-border overflow-auto flex flex-col justify-start mx-2 px-1 pt-1 pb-5">
+            {children}
+          </div>
         </div>
-      </div>
-    </aside>,
+      </aside>
+    </FocusTrap>,
     document.body
   );
 }
