283ea4a924cf54a68319135555834eb1f18b0e5b
Maxime Souillat <maxime@beaucouplus.com>
2020-05-20 12:07:37 +0200

    Add custom header per view (#35)
    * add custom header per view > extracted header and created CurrentViewMenu component
    * add custom header per view > revamped menu - works as expected
    * add custom header per view > updated menu to make sure width is always the same in ViewMenu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useState, useContext, useEffect } from "react";
+// packages
+import React, { useState, useEffect } from "react";
 import dayjs from "dayjs";
+
+// local files
+import Header from "./Header";
 import YearView from "./YearView";
 import MonthView from "./MonthView";
-import { Button, OutlineButton } from "./Button";
-import Modal from "./Modal";
-import DayModal from "./DayModal";
-import { EventContext, EventStore } from "./EventContext";
+import { EventStore } from "./EventContext";
 
 function App() {
   const currentDate = new Date();
@@ -22,7 +23,14 @@ function App() {
   return (
     <EventStore>
       <div className="w-screen h-screen flex flex-col">
-        <Header year={year} onSetYear={setYear} onSelectView={selectView} />
+        <Header
+          year={year}
+          onSetYear={setYear}
+          currentView={currentView}
+          onSelectView={selectView}
+          startOfMonth={startOfMonth}
+          onSetStartOfMonth={setStartOfMonth}
+        />
         <CalendarView
           view={currentView}
           year={year}
@@ -33,70 +41,13 @@ function App() {
   );
 }
 
-function Header({ year, onSetYear, onSelectView }) {
-  const { events } = useContext(EventContext);
-  const [showModal, setShowModal] = useState(false);
-
-  const previousYear = () => onSetYear(year - 1);
-  const nextYear = () => onSetYear(year + 1);
-  const today = new Date(dayjs().startOf("day"));
-  const todayEvents = events[dayjs().startOf("day").format("YYYY-MM-DD")];
-
-  const openModal = () => setShowModal(true);
-  const closeModal = () => setShowModal(false);
-
-  const headerStyle = `flex flew-row
-                       h-auto
-                       font-semibold text-gray-700`;
-  return (
-    <div className={headerStyle}>
-      <div className="flex items-center border-r border-gray-500 px-10 py-2">
-        <h1 className="flex flex-row  items-center text-2xl tracking-wide">
-          <i className="gg-calendar-today mr-3"></i>Calendar
-        </h1>
-      </div>
-      <div className="flex flex-row items-center px-10 space-x-5">
-        <ul className="flex flex-row h-full items-center align-middle space-x-2 pr-10 border-r border-gray-500">
-          <li>
-            <Button callBack={() => onSelectView("YearView")}>Y</Button>
-          </li>
-          <li>
-            <Button callBack={() => onSelectView("MonthView")}>M</Button>
-          </li>
-        </ul>
-        <h2 className="text-xl tracking-wide">{year}</h2>
-        <ul className="flex flex-row h-full items-center align-middle space-x-2 pr-10 border-r border-gray-500">
-          <li>
-            <Button callBack={previousYear}>
-              <i className="gg-arrow-left-o "></i>
-            </Button>
-          </li>
-          <li>
-            <Button callBack={nextYear}>
-              <i className="gg-arrow-right-o"></i>
-            </Button>
-          </li>
-        </ul>
-        <ul className="flex flex-row h-full items-center align-middle">
-          <li>
-            <OutlineButton callBack={openModal}>Today</OutlineButton>
-            <Modal showModal={showModal} onCloseModal={closeModal}>
-              <DayModal date={today} events={todayEvents} />
-            </Modal>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-}
-
 function CalendarView({ view, year, startOfMonth }) {
   const views = {
     YearView,
     MonthView,
   };
 
-  const props = {
+  const viewProps = {
     YearView: { year },
     MonthView: { startOfMonth },
   };
@@ -105,7 +56,7 @@ function CalendarView({ view, year, startOfMonth }) {
 
   return (
     <div className="flex-grow">
-      <CurrentView {...props[view]} />
+      <CurrentView {...viewProps[view]} />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,117 @@
+import React, { useContext, useState } from "react";
+import { Button, OutlineButton } from "./Button";
+import { EventContext } from "./EventContext";
+import dayjs from "dayjs";
+import Modal from "./Modal";
+import DayModal from "./DayModal";
+
+function Header({
+  year,
+  onSetYear,
+  startOfMonth,
+  onSetStartOfMonth,
+  currentView,
+  onSelectView,
+}) {
+  const { events } = useContext(EventContext);
+  const [showModal, setShowModal] = useState(false);
+
+  const previousYear = () => onSetYear(year - 1);
+  const nextYear = () => onSetYear(year + 1);
+
+  const previousMonth = () =>
+    onSetStartOfMonth(dayjs(startOfMonth).subtract(1, "month"));
+  const nextMonth = () =>
+    onSetStartOfMonth(dayjs(startOfMonth).add(1, "month"));
+  const today = new Date(dayjs().startOf("day"));
+  const todayEvents = events[dayjs().startOf("day").format("YYYY-MM-DD")];
+
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+
+  const headerStyle = `flex flew-row
+                       w-full h-auto
+                       font-semibold text-gray-700`;
+  return (
+    <div className={headerStyle}>
+      <div className="flex items-center border-r border-gray-500 px-10 py-2">
+        <h1 className="flex flex-row  items-center text-2xl tracking-wide">
+          <i className="gg-calendar-today mr-3"></i>Calendar
+        </h1>
+      </div>
+      <div className="flex flex-row items-center px-10 space-x-5">
+        <ul className="flex flex-row h-full items-center align-middle space-x-2 pr-10 border-r border-gray-500">
+          <li>
+            <Button callBack={() => onSelectView("YearView")}>Y</Button>
+          </li>
+          <li>
+            <Button callBack={() => onSelectView("MonthView")}>M</Button>
+          </li>
+        </ul>
+
+        <CurrentViewMenu
+          currentView={currentView}
+          year={year}
+          onPreviousYear={previousYear}
+          onNextYear={nextYear}
+          startOfMonth={startOfMonth}
+          onPreviousMonth={previousMonth}
+          onNextMonth={nextMonth}
+        />
+      </div>
+      <div className="flex flex-grow">
+        <ul className="flex flex-row h-full items-center align-middle pr-5 ">
+          <li>
+            <OutlineButton callBack={openModal}>Today</OutlineButton>
+            <Modal showModal={showModal} onCloseModal={closeModal}>
+              <DayModal date={today} events={todayEvents} />
+            </Modal>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function CurrentViewMenu({
+  currentView,
+  year,
+  onPreviousYear,
+  onNextYear,
+  startOfMonth,
+  onPreviousMonth,
+  onNextMonth,
+}) {
+  const viewProps = {
+    YearView: { period: year, previous: onPreviousYear, next: onNextYear },
+    MonthView: {
+      period: dayjs(startOfMonth).format("MMMM YYYY"),
+      previous: onPreviousMonth,
+      next: onNextMonth,
+    },
+  };
+
+  return <ViewMenu {...viewProps[currentView]} />;
+}
+
+function ViewMenu({ period, previous, next }) {
+  return (
+    <div className="flex space-x-6 px-5 h-full items-center align-middle border-r border-gray-500">
+      <ul className="flex flex-row space-x-3">
+        <li>
+          <Button callBack={previous}>
+            <i className="gg-arrow-left-o "></i>
+          </Button>
+        </li>
+        <li>
+          <Button callBack={next}>
+            <i className="gg-arrow-right-o"></i>
+          </Button>
+        </li>
+      </ul>
+      <h2 className="w-48 text-xl tracking-wide ">{period}</h2>
+    </div>
+  );
+}
+
+export default Header;
