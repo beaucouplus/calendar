53e563c19ad57e66ace4bb1b1721131950082bf8
Maxime Souillat <maxime@beaucouplus.com>
2020-06-01 23:40:55 +0200

    improve form > extracted timeInput component

--- src/EventForm.js
+++ src/EventForm.js
@@ -9,9 +9,6 @@ function EventForm({ events, date, display, onAddEvent, onClose }) {
   const titleInput = useRef();
 
   const styles = {
-    valid: "focus:text-blue-700 focus:bg-blue-100 focus:border-blue-800",
-    invalid:
-      "bg-red-100 border-red-800 focus:text-red-700 focus:bg-red-100 focus:border-red-800",
     input: `bg-gray-100 appearance-none
             border-2 border-gray-400 rounded
             w-full
@@ -19,40 +16,14 @@ function EventForm({ events, date, display, onAddEvent, onClose }) {
             text-gray-700 leading-tight
             hover:border-blue-800 hover:bg-blue-100 hover:text-blue-700
             focus:outline-none focus:text-blue-700 focus:bg-blue-100 focus:border-blue-800`,
-    inputInGroup: `flex flex-grow
-                 bg-gray-100 appearance-none
-                 py-2 px-4
-                 text-gray-700 leading-tight
-                 border-2 border-gray-400
-                 rounded-l rounded-r-none
-                 hover:bg-blue-100 hover:text-blue-700 hover:border-blue-800
-                 focus:outline-none
-                 `,
-    inputButton: `flex justify-center bg-gray-400
-                  py-2 px-4
-                  border-r-2 border-b-2 border-t-2 border-gray-400
-                  rounded-r rounded-l-none
-                  text-gray-800 text-center
-                  group-hover:bg-blue-800 group-hover:border-blue-800 group-hover:text-white
-                  hover:bg-blue-800 hover:border-blue-800
-                  focus:outline-none focus:bg-blue-800 focus:border-blue-800 focus:text-white`,
   };
 
   const [timeInput, setTimeInput] = useState("12:00");
-  const [timeInputStyle, setTimeInputStyle] = useState(styles.valid);
   const [hours, minutes] = timeInput.split(":");
 
   const [valid, validate] = useState(false);
-  const [timePickerShown, setTimePickerShown] = useState(false);
 
   const renderTwoDigits = (int) => (int < 10 ? `0${int}` : `${int}`);
-
-  const toggleTimePicker = () => setTimePickerShown(!timePickerShown);
-
-  const close = () => {
-    setTimePickerShown(false);
-    onClose();
-  };
 
   const chooseHour = (event) => {
     setTimeInput(`${renderTwoDigits(event.currentTarget.value)}:${minutes}`);
@@ -60,7 +31,6 @@ function EventForm({ events, date, display, onAddEvent, onClose }) {
 
   const chooseMinutes = (event) => {
     setTimeInput(`${hours}:${renderTwoDigits(event.currentTarget.value)}`);
-    toggleTimePicker();
   };
 
   const selectText = () => titleInput.current.select();
@@ -68,29 +38,6 @@ function EventForm({ events, date, display, onAddEvent, onClose }) {
   const handleChange = (event) => {
     setTimeInput(event.target.value);
   };
-
-  useEffect(() => {
-    const validateInput = (input) => {
-      const [currentHours, currentMinutes] = input.split(":");
-
-      const hoursNum = Number(currentHours);
-      const minutesNum = Number(currentMinutes);
-      if (
-        input.length === 5 &&
-        hoursNum >= 0 &&
-        hoursNum < 24 &&
-        minutesNum >= 0 &&
-        minutesNum <= 55
-      ) {
-        validate(true);
-        setTimeInputStyle(styles.valid);
-      } else {
-        validate(false);
-        setTimeInputStyle(styles.invalid);
-      }
-    };
-    validateInput(timeInput);
-  }, [timeInput, styles.invalid, styles.valid]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -127,35 +74,21 @@ function EventForm({ events, date, display, onAddEvent, onClose }) {
               onFocus={selectText}
             />
           </div>
-          <div className="mt-2">
-            <EventLabel>Start time</EventLabel>
-            <div className="flex group">
-              <input
-                className={`${styles.inputInGroup} ${timeInputStyle}`}
-                type="text"
-                value={timeInput}
-                onChange={(e) => handleChange(e)}
-              />
-              <Button
-                callBack={() => toggleTimePicker()}
-                css={styles.inputButton}
-                withFocus={false}
-              >
-                <i className="gg-chevron-down"></i>
-              </Button>
-            </div>
-            <TimePicker
-              eventHour={Number(hours)}
-              eventMinutes={Number(minutes)}
-              onChooseHour={(event) => chooseHour(event)}
-              onChooseMinutes={(event) => chooseMinutes(event)}
-              display={timePickerShown}
-            />
-          </div>
+          <TimeInput
+            timeInput={timeInput}
+            onChooseHour={(event) => chooseHour(event)}
+            onChooseMinutes={(event) => chooseMinutes(event)}
+            hours={hours}
+            minutes={minutes}
+            onHandleChange={(e) => handleChange(e)}
+            onValidate={() => validate(true)}
+            onInvalidate={() => validate(false)}
+          />
+
           <div className="flex mt-6 space-x-2">
             <BlueSubmitButton />
             <OutlineButton
-              callBack={close}
+              callBack={onClose}
               ariaLabel="close form"
               ariaLabelledBy="close-form"
             >
@@ -175,6 +108,101 @@ EventForm.propTypes = exact({
   onClose: PropTypes.func.isRequired,
   events: PropTypes.array,
 });
+
+function TimeInput({
+  timeInput,
+  onHandleChange,
+  hours,
+  minutes,
+  onChooseHour,
+  onChooseMinutes,
+  onValidate,
+  onInvalidate,
+}) {
+  const [timePickerShown, setTimePickerShown] = useState(false);
+  const toggleTimePicker = () => setTimePickerShown(!timePickerShown);
+
+  const styles = {
+    inputInGroup: `flex flex-grow
+                 bg-gray-100 appearance-none
+                 py-2 px-4
+                 text-gray-700 leading-tight
+                 border-2 border-gray-400
+                 rounded-l rounded-r-none
+                 hover:bg-blue-100 hover:text-blue-700 hover:border-blue-800
+                 focus:outline-none
+                 `,
+    inputButton: `flex justify-center bg-gray-400
+                  py-2 px-4
+                  border-r-2 border-b-2 border-t-2 border-gray-400
+                  rounded-r rounded-l-none
+                  text-gray-800 text-center
+                  group-hover:bg-blue-800 group-hover:border-blue-800 group-hover:text-white
+                  hover:bg-blue-800 hover:border-blue-800
+                  focus:outline-none focus:bg-blue-800 focus:border-blue-800 focus:text-white`,
+    valid: "focus:text-blue-700 focus:bg-blue-100 focus:border-blue-800",
+    invalid:
+      "bg-red-100 border-red-800 focus:text-red-700 focus:bg-red-100 focus:border-red-800",
+  };
+
+  const [timeInputStyle, setTimeInputStyle] = useState(styles.valid);
+
+  useEffect(() => {
+    const validateInput = (timeInput) => {
+      const [currentHours, currentMinutes] = timeInput.split(":");
+
+      const hoursNum = Number(currentHours);
+      const minutesNum = Number(currentMinutes);
+      if (
+        timeInput.length === 5 &&
+        hoursNum >= 0 &&
+        hoursNum < 24 &&
+        minutesNum >= 0 &&
+        minutesNum <= 59
+      ) {
+        onValidate();
+        setTimeInputStyle(styles.valid);
+      } else {
+        onInvalidate();
+        setTimeInputStyle(styles.invalid);
+      }
+    };
+    validateInput(timeInput);
+  }, [timeInput, styles.invalid, styles.valid, onValidate, onInvalidate]);
+
+  const chooseMinutes = (event) => {
+    onChooseMinutes(event);
+    toggleTimePicker();
+  };
+
+  return (
+    <div className="mt-2">
+      <EventLabel>Start time</EventLabel>
+      <div className="flex group">
+        <input
+          className={`${styles.inputInGroup} ${timeInputStyle}`}
+          type="text"
+          value={timeInput}
+          onChange={onHandleChange}
+        />
+        <Button
+          callBack={() => toggleTimePicker()}
+          css={styles.inputButton}
+          withFocus={false}
+        >
+          <i className="gg-chevron-down"></i>
+        </Button>
+      </div>
+      <TimePicker
+        eventHour={Number(hours)}
+        eventMinutes={Number(minutes)}
+        onChooseHour={onChooseHour}
+        onChooseMinutes={(event) => chooseMinutes(event)}
+        display={timePickerShown}
+      />
+    </div>
+  );
+}
 
 function EventLabel({ children }) {
   return (
