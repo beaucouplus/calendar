b1bed353784b0c881b0b9c91ff402994bcd6c555
Maxime Souillat <maxime@beaucouplus.com>
2020-06-27 23:26:02 +0200

    add all Day events > finished reducer. Extracted many things from the component. Changed linter config so many line width changes

--- src/EventForm.js
+++ src/EventForm.js
@@ -8,6 +8,73 @@ import "react-day-picker/lib/style.css";
 import { Button, OutlineButton, BlueSubmitButton } from "./Button";
 import TimePicker from "./TimePicker";
 
+const ISOTimeFormat = `YYYY-MM-DDTHH:mm:00+02:00`;
+const renderTwoDigits = (int) => (int < 10 ? `0${int}` : `${int}`);
+
+class timeHelper {
+  constructor(formats) {
+    this.formats = formats;
+  }
+
+  toYear = (time) => dayjs(time).format(this.formats.year);
+
+  toHumanReadableTime = (hours, minutes) => `${renderTwoDigits(Number(hours))}:${renderTwoDigits(Number(minutes))}`;
+
+  updateHour = (currentTime, hour) => dayjs(currentTime, this.formats.iso).hour(hour).format(this.formats.iso);
+
+  updateMinute = (currentTime, minute) => dayjs(currentTime, this.formats.iso).minute(minute).format(this.formats.iso);
+
+  updateTime = (currentTime, hour, minute) =>
+    dayjs(currentTime, this.formats.iso).hour(hour).minute(minute).format(this.formats.iso);
+}
+
+const eventFormReducer = produce((draft, action) => {
+  const timeManager = new timeHelper({ iso: ISOTimeFormat, year: "YYYY-MM-DD" });
+  let [hours, minutes] = draft.startTimeInput.inputValue.split(":");
+  switch (action.type) {
+    case "toggleDate":
+      draft.isAllDayEvent = !draft.isAllDayEvent;
+      break;
+    case "addEndDateFromInput":
+      draft.event.end.date = action.name;
+      break;
+    case "addEndDateFromPicker":
+      draft.event.end.date = timeManager.toYear(action.name);
+      break;
+    case "addStartHourFromTimePicker":
+      draft.event.start.datetime = timeManager.updateHour(draft.event.start.datetime, action.name);
+      hours = action.name;
+      draft.startTimeInput.inputValue = timeManager.toHumanReadableTime(hours, minutes);
+      break;
+    case "addStartMinutesFromTimePicker":
+      draft.event.start.datetime = timeManager.updateMinute(draft.event.start.datetime, action.name);
+      minutes = action.name;
+      draft.startTimeInput.inputValue = timeManager.toHumanReadableTime(hours, minutes);
+      break;
+    case "addStartTimeFromTimeInput":
+      draft.startTimeInput.inputValue = action.name;
+      break;
+    case "validateStartTime":
+      draft.startTimeInput.valid = true;
+      draft.event.start.datetime = timeManager.updateTime(draft.event.start.datetime, hours, minutes);
+      break;
+    case "invalidateStartTime":
+      draft.startTimeInput.valid = false;
+      break;
+    case "submit":
+      if (draft.isAllDayEvent) {
+        draft.event.start.datetime = null;
+        draft.event.end.datetime = null;
+      } else {
+        draft.event.start.date = null;
+        draft.event.end.date = null;
+      }
+      draft.readyForSubmit = true;
+      break;
+    default:
+  }
+});
+
 function EventForm({ events, date, display, onAddEvent, onClose }) {
   const titleInput = useRef();
 
@@ -21,102 +88,49 @@ function EventForm({ events, date, display, onAddEvent, onClose }) {
             focus:outline-none focus:text-blue-700 focus:bg-blue-100 focus:border-blue-800`,
   };
 
-  // TODO finish reducer
-  // update submit
-  //
-  const reducer = produce((draft, action) => {
-    console.log(action.type, action.name);
-    switch (action.type) {
-      case "toggle":
-        draft.isAllDayEvent = !draft.isAllDayEvent;
-        break;
-      case "addEndDateFromInput":
-        draft.event.end.date = action.name;
-        break;
-      case "addEndDateFromPicker":
-        draft.event.end.date = dayjs(action.name).format("YYYY-MM-DD");
-        break;
-      default:
-    }
-  });
-
-  const [eventForm, dispatch] = useReducer(reducer, {
+  const defaultEventStartTime = dayjs(date).format(ISOTimeFormat);
+  const defaultEvent = {
     event: {
       start: {
         date: date,
-        datetime: null,
+        datetime: defaultEventStartTime,
       },
       end: {
         date: date,
-        datetime: null,
+        datetime: dayjs(defaultEventStartTime).add(1, "hour").format(ISOTimeFormat),
       },
       title: "No title",
     },
     isAllDayEvent: false,
+    isValid: false,
+    readyForSubmit: false,
+    startTimeInput: {
+      method: "input",
+      inputValue: "12:00",
+      valid: true,
+    },
+  };
+
+  const [eventForm, dispatch] = useReducer(eventFormReducer, defaultEvent);
+
+  useEffect(() => {
+    if (eventForm.readyForSubmit) {
+      onAddEvent(eventForm.event);
+      onClose();
+    }
   });
-  const [timeInput, setTimeInput] = useState("12:00");
-  const [hours, minutes] = timeInput.split(":");
-
-  const [valid, validate] = useState(false);
-
-  const renderTwoDigits = (int) => (int < 10 ? `0${int}` : `${int}`);
-
-  const chooseHour = (event) => {
-    setTimeInput(`${renderTwoDigits(event.currentTarget.value)}:${minutes}`);
-  };
-
-  const chooseMinutes = (event) => {
-    setTimeInput(`${hours}:${renderTwoDigits(event.currentTarget.value)}`);
-  };
-
-  const handleTimeInputChange = (event) => {
-    setTimeInput(event.target.value);
-  };
 
   const selectText = () => titleInput.current.select();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!valid) return;
-
-    if (eventForm.isAllDayEvent) {
-      onAddEvent({
-        start: {
-          date: date,
-          datetime: null,
-        },
-        end: {
-          date: eventForm.date,
-          datetime: null,
-        },
-        title: titleInput.current.value,
-      });
-    } else {
-      const timeFormat = `YYYY-MM-DDT${timeInput}:00+02:00`;
-      const eventTime = dayjs(date).format(timeFormat);
-      onAddEvent({
-        start: {
-          date: null,
-          datetime: eventTime,
-        },
-        end: {
-          date: null,
-          datetime: dayjs(eventTime).add(1, "hour").format(timeFormat),
-        },
-        title: titleInput.current.value,
-      });
-    }
-    onClose();
+    dispatch({ type: "submit" });
   };
 
   return (
     <>
       {display && (
-        <form
-          action=""
-          className="mt-10 w-full bg-white p-6 rounded-lg"
-          onSubmit={handleSubmit}
-        >
+        <form action="" className="mt-10 w-full bg-white p-6 rounded-lg" onSubmit={handleSubmit}>
           <div id="form-title" className="">
             <h3 className="flex items-center leading-relaxed text-xl tracking-wider text-blue-700 font-medium">
               <i className="gg-add-r mr-2"></i> New Event
@@ -135,7 +149,7 @@ function EventForm({ events, date, display, onAddEvent, onClose }) {
           <div className="mt-4">
             <Toggle
               checked={eventForm.isAllDayEvent}
-              onCheck={() => dispatch({ type: "toggle" })}
+              onCheck={() => dispatch({ type: "toggleDate" })}
               checkedTitle="All Day Event"
               unCheckedTitle="All Day Event?"
             />
@@ -143,37 +157,42 @@ function EventForm({ events, date, display, onAddEvent, onClose }) {
           <div className="mt-2 border border-gray-300 rounded p-4">
             {eventForm.isAllDayEvent ? (
               <AllDayEventInput
-                onInputChange={(e) =>
-                  dispatch({ type: "addEndDateFromInput", name: e })
-                }
-                onPickerChange={(day) =>
-                  dispatch({ type: "addEndDateFromPicker", name: day })
-                }
+                onInputChange={(event) => dispatch({ type: "addEndDateFromInput", name: event })}
+                onPickerChange={(day) => dispatch({ type: "addEndDateFromPicker", name: day })}
                 css={styles.input}
-                inputValue={eventForm.event.end.date}
+                inputValue={eventForm.event.end.date ? eventForm.event.end.date : ""}
                 date={date}
               />
             ) : (
               <TimeInput
                 title={"Start time"}
-                timeInput={timeInput}
-                onChooseHour={(event) => chooseHour(event)}
-                onChooseMinutes={(event) => chooseMinutes(event)}
-                hours={hours}
-                minutes={minutes}
-                onHandleChange={(e) => handleTimeInputChange(e)}
-                onValidate={() => validate(true)}
-                onInvalidate={() => validate(false)}
+                timeInput={eventForm.startTimeInput.inputValue}
+                onChooseHour={(event) =>
+                  dispatch({
+                    type: "addStartHourFromTimePicker",
+                    name: event.target.value,
+                  })
+                }
+                onChooseMinutes={(event) =>
+                  dispatch({
+                    type: "addStartMinutesFromTimePicker",
+                    name: event.target.value,
+                  })
+                }
+                onHandleChange={(event) =>
+                  dispatch({
+                    type: "addStartTimeFromTimeInput",
+                    name: event.target.value,
+                  })
+                }
+                onValidate={() => dispatch({ type: "validateStartTime" })}
+                onInvalidate={() => dispatch({ type: "invalidateStartTime" })}
               />
             )}
           </div>
           <div className="flex mt-6 space-x-2">
             <BlueSubmitButton />
-            <OutlineButton
-              callBack={onClose}
-              ariaLabel="close form"
-              ariaLabelledBy="close-form"
-            >
+            <OutlineButton callBack={onClose} ariaLabel="close form" ariaLabelledBy="close-form">
               Cancel
             </OutlineButton>
           </div>
@@ -191,13 +210,7 @@ EventForm.propTypes = exact({
   events: PropTypes.array,
 });
 
-function AllDayEventInput({
-  onInputChange,
-  onPickerChange,
-  css,
-  inputValue,
-  date,
-}) {
+function AllDayEventInput({ onInputChange, onPickerChange, css, inputValue, date }) {
   const disabledInput = `bg-white appearance-none
   border-2 border-gray-300 rounded
   w-full
@@ -231,12 +244,7 @@ function AllDayEventInput({
           onChange={onInputChange}
           onClick={() => setDatePickerShown(!datePickerShown)}
         />
-        <DatePickerOverlay
-          show={datePickerShown}
-          onDayClick={handleDayClick}
-          inputValue={inputValue}
-          onClose={close}
-        />
+        <DatePickerOverlay show={datePickerShown} onDayClick={handleDayClick} inputValue={inputValue} onClose={close} />
       </div>
     </div>
   );
@@ -250,27 +258,13 @@ function DatePickerOverlay({ show, onDayClick, inputValue, date, onClose }) {
           className="absolute right-0 top-20 mt-1 bg-white border border-gray-300 rounded shadow"
           onMouseLeave={onClose}
         >
-          <DayPicker
-            onDayClick={onDayClick}
-            value={inputValue}
-            disabledDays={[{ before: dayjs(date).toDate() }]}
-          />
+          <DayPicker onDayClick={onDayClick} value={inputValue} disabledDays={[{ before: dayjs(date).toDate() }]} />
         </div>
       )}
     </>
   );
 }
-function TimeInput({
-  title,
-  timeInput,
-  onHandleChange,
-  hours,
-  minutes,
-  onChooseHour,
-  onChooseMinutes,
-  onValidate,
-  onInvalidate,
-}) {
+function TimeInput({ title, timeInput, onHandleChange, onChooseHour, onChooseMinutes, onValidate, onInvalidate }) {
   const [timePickerShown, setTimePickerShown] = useState(false);
   const toggleTimePicker = () => setTimePickerShown(!timePickerShown);
 
@@ -293,11 +287,11 @@ function TimeInput({
                   hover:bg-blue-800 hover:border-blue-800
                   focus:outline-none focus:bg-blue-800 focus:border-blue-800 focus:text-white`,
     valid: "focus:text-blue-700 focus:bg-blue-100 focus:border-blue-800",
-    invalid:
-      "bg-red-100 border-red-800 focus:text-red-700 focus:bg-red-100 focus:border-red-800",
+    invalid: "bg-red-100 border-red-800 focus:text-red-700 focus:bg-red-100 focus:border-red-800",
   };
 
   const [timeInputStyle, setTimeInputStyle] = useState(styles.valid);
+  const [hours, minutes] = timeInput && timeInput.split(":");
 
   useEffect(() => {
     const validateInput = (timeInput) => {
@@ -305,13 +299,7 @@ function TimeInput({
 
       const hoursNum = Number(currentHours);
       const minutesNum = Number(currentMinutes);
-      if (
-        timeInput.length === 5 &&
-        hoursNum >= 0 &&
-        hoursNum < 24 &&
-        minutesNum >= 0 &&
-        minutesNum <= 59
-      ) {
+      if (timeInput.length === 5 && hoursNum >= 0 && hoursNum < 24 && minutesNum >= 0 && minutesNum <= 59) {
         onValidate();
         setTimeInputStyle(styles.valid);
       } else {
@@ -337,11 +325,7 @@ function TimeInput({
           value={timeInput}
           onChange={onHandleChange}
         />
-        <Button
-          callBack={() => toggleTimePicker()}
-          css={styles.timePickerToggle}
-          withFocus={false}
-        >
+        <Button callBack={() => toggleTimePicker()} css={styles.timePickerToggle} withFocus={false}>
           <i className="gg-chevron-down"></i>
         </Button>
       </div>
@@ -357,11 +341,7 @@ function TimeInput({
 }
 
 function EventLabel({ children }) {
-  return (
-    <label className="block w-full leading-relaxed tracking-wider text-blue-700 ">
-      {children}
-    </label>
-  );
+  return <label className="block w-full leading-relaxed tracking-wider text-blue-700 ">{children}</label>;
 }
 
 function Toggle({ checked, onCheck, checkedTitle, unCheckedTitle }) {
@@ -382,18 +362,12 @@ function Toggle({ checked, onCheck, checkedTitle, unCheckedTitle }) {
       onClick={onCheck}
     >
       <span className="relative">
-        <span
-          className={`block w-10 h-6 ${
-            checked ? "bg-blue-600" : "bg-gray-400"
-          } rounded-full shadow-inner`}
-        ></span>
+        <span className={`block w-10 h-6 ${checked ? "bg-blue-600" : "bg-gray-400"} rounded-full shadow-inner`}></span>
         <span className={`${styles.toggle} ${checked && styles.checked}`}>
           <input type="checkbox" className="absolute opacity-0 w-0 h-0" />
         </span>
       </span>
-      <span className="ml-3 text-md text-gray-700">
-        {checked ? checkedTitle : unCheckedTitle}
-      </span>
+      <span className="ml-3 text-md text-gray-700">{checked ? checkedTitle : unCheckedTitle}</span>
     </label>
   );
 }
